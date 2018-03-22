4ba0615c59ea4d2e49b75cad7ac552b8370abb62
corey2915 <32797309+corey2915@users.noreply.github.com>
2018-03-22 17:17:08 -0400

    finsh

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
+console.log("hi")
 var attitude=null;
     tools=null;
     wear=null;
-    pageTitle = document.getElementById('page-title'),
-    pageTitleText = pageTitle.innerHTML
-    tryAgain=document.createElement("tryAgain");
-    quizWrapper=document.createElement("quizWrapper");
-    result=document.createElement("result");
-    formSudmit=document.createElement("formSudmit");
+    pagetitle = document.getElementById('pagetitle');
+    pageTitleText = pagetitle.innerHTML
+    tryAgain=document.getElementById('tryAgain');
+    quizWrapper=document.getElementById('quizWrapper');
+    result=document.getElementById('result');
+    fromSubmit=document.getElementById('fromSubmit');
     tryAgain.addEventListener("click", resetQuiz);
-    formSudmit.addEventListener("click",processResults );
+    fromSubmit.addEventListener("click",processResults );
 
 function processResults(){
 attitude= document.querySelector('input[name="attitude"]:checked');
@@ -22,22 +23,23 @@ if(attitude== null){
   alert("Complete all questions before continuing.");
 }else{
   var personality = getPersonality()
+console.log(personality)
   quizWrapper.style.display = "none"
-  formSubmit.style.display = "none"
+  fromSubmit.style.display = "none"
   result.style.display = "block"
   tryAgain.style.display = "block"
-  if(personality == 0){
-    pageTitle.innerHTML = "You're sata"
+  if(personality == "sata"){
+    pagetitle.innerHTML = "You're sata"
     result.style.backgroundImage = "url('img/stat.jpg')"
-  }else if(personality == 1){
-    pageTitle.innerHTML = "You're mayble"
+  }else if(personality == "mayble"){
+    pagetitle.innerHTML = "You're mayble"
     result.style.backgroundImage = "url('img/mayble.png')"
-  }else if(personality == 2){
-  pageTitle.innerHTML = "You're dipper"
+  }else if(personality == "dipper"){
+  pagetitle.innerHTML = "You're dipper"
   result.style.backgroundImage = "url('img/dipper.png')"
   }
-  else if(personality == 3){
-    pageTitle.innerHTML = "You're wendy"
+  else if(personality == "wendy"){
+    pagetitle.innerHTML = "You're wendy"
     result.style.backgroundImage = "url('img/wendy.jpg')"
   }
 }
@@ -46,69 +48,75 @@ if(attitude== null){
 
 function getPersonality(){
 var score=0
-if(attitude.id ==="outgoing"){
+if(attitude.id ==="Outgoing"){
 score += 0;
 }
-else if (attitude.id==="shellfish") {
+else if (attitude.id==="Shellfish") {
 score += 2;
 }
 else if (attitude.id==="Careful") {
 score += 1;
 }
-else if (attitude.id==="Don't care") {
+else if (attitude.id==="Don’t care") {
 score += 3;
 }
-
+console.log("attitude score");
+console.log(score);
 if(tools.id ==="Grappling hook"){
 score += 0;
 }
 else if (tools.id==="Money") {
 score += 2;
 }
-else if (tools.id==="AXE") {
+else if (tools.id==="Axe") {
 score += 1;
 }
-else if (tools.id==="book") {
+else if (tools.id==="Book") {
 score += 3;
 }
-
-if(wear.id ==="sweater"){
+console.log("tools score");
+console.log(score);
+if(wear.id ==="Sweater"){
 score += 0;
 }
-else if (wear.id==="boots") {
+else if (wear.id==="Boots") {
 score += 2;
 }
-else if (wear.id==="fez") {
+else if (wear.id==="Fez") {
 score += 1;
 }
-else if (wear.id==="Vebook") {
+else if (wear.id==="Vest") {
 score += 3;
 }
+console.log("end score");
+console.log(score);
 if(score<=1){
-  return sata
+  return "sata"
 }
-else if(score >=2||score<=3) {
-  return mayble
+else if(score >=2 &&score<=3) {
+  return "mayble"
 }
-else if(score>=4||score<= 5) {
-  return dipper
+else if(score>=4&&score<= 5) {
+  return "dipper"
 }
-else if(score>=6||score<= 7) {
-  return wendy
+else if(score>=6&&score<= 10) {
+  return "wendy"
+
 }
+
 
 }
 
 function resetQuiz(){
-	pageTitle.innerHTML =  pageTitleText
+	pagetitle.innerHTML =  pageTitleText
 	quizWrapper.style.display = "flex"
 	result.style.display = "none"
     tryAgain.style.display = "none"
-    formSubmit.style.display = "block"
-    time.checked = "false"
-    asking.checked = "false"
-    doing.checked = "false"
-    time = null
-    asking = null
-    doing = null
+    fromSubmit.style.display = "block"
+    attitude.checked = false
+    tools.checked = false
+    wear.checked = false
+    attitude=null;
+        tools=null;
+        wear=null;
 }
